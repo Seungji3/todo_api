833c07bd0ef06704991b553c4f7ccf7c6b4ab2c3
DESKTOP-1QT0PI9\a0106 <seungjipark77@naver.com>
2023-11-15 11:32:54 +0900

    feat: JpaRepository 생성

---
+++ src/main/java/data/repository/TodoRepository.java
@@ -0,0 +1,8 @@
+package data.repository;
+
+import data.entity.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+
+}
